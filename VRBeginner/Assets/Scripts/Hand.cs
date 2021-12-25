7fe6ad3440740ed8a81bf64525a6bdc4dd126ee0
naomi-rc <35537740+naomi-rc@users.noreply.github.com>
2021-12-24 23:06:43 -0500

    add animation to right hand; add hand controllers

---
+++ VRBeginner/Assets/Scripts/Hand.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    public float speed;
+
+    Animator animator;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void SetTrigger(float value)
+    {
+        gripTarget = value;
+
+    }
+
+    internal void SetGrip(float value)
+    {
+        triggerTarget = value;
+    }
+
+    void AnimateHand()
+    {
+        if(gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        }
+
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        }
+    }
+}
---
+++ VRBeginner/Assets/Scripts/HandController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(ActionBasedController))]
+public class HandController : MonoBehaviour
+{
+    public Hand hand;
+
+    ActionBasedController controller;
+    
+    void Start()
+    {
+        controller = GetComponent<ActionBasedController>();
+    }
+
+    void Update()
+    {
+        hand.SetGrip(controller.selectAction.action.ReadValue<float>());
+        hand.SetTrigger(controller.activateAction.action.ReadValue<float>());
+    }
+}
